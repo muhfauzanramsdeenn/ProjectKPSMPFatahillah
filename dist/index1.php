eca7240bc9c9e1e4b3e373827016cb8e62e38d79
muhfauzanramsdenn <fauzanjr1998@gmail.com>
2024-07-15 05:32:00 +0700

    pertama commit

---
+++ dist/index1.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+ <!-- boootstrap -->
+    <link href="vendor/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+      <link href="vendor/css/bootstrap/bootstrap.css" rel="stylesheet">
+
+    <!-- icon dan fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- tema css -->
+    <link href="src/plugin/bootstrap/css/tampilan.css" rel="stylesheet">
+	<title>E-Catalogue SMP Fatahillah Ciledug</title>
+</head>
+<body>
+
+
+<!-- Menu -->
+    <nav class="navbar navbar-default navbar-fixed-top navbar-custom  ">
+      <div class="container">
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">navigation</span> Menu <i class="fa fa-bars"></i>
+          </button>
+          <a class="navbar-brand" href="#">
+          <img src="images/Logo SMP Fatahillah.png" alt="Logo" class ="logo-left" style="height: 35px; width: 40px; margin-right: 10px;">
+          <a class="navbar-brand">E-Catalogue Perpustakaan SMP Fatahillah Ciledug</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="page-scroll">
+              <a href="index1.php">Beranda</a>
+            </li>
+            <li class="page-scroll">
+              <a href="index.php">Masuk</a>
+            </li>
+            <li class="page-scroll">
+              <a href="#tentang">Tentang</a>
+            </li>
+          </ul>
+        </div>
+
+ 
+
+      </div>
+    </nav>	
+
+               <!-- Header atau gambar-->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="images/pramuka.jpg" alt="First slide">
+        </div>
+        <div class="item">
+          <img class="second-slide" src="images/smp fatahillah 2.jpg" alt="Second slide">
+        </div>
+        <div class="item">
+        <video width="1300" height="800" controls>
+          <source src="images/SMP Fatahillah goes to Bandung.mp4" type ="video/mp4" alt="Third slide">
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+       
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+   
+     <!-- Maps -->
+     <div class="map-container">
+     <a class="title">Lokasi SMP Fatahillah Ciledug </a> 
+
+        <!-- Kode iframe dari Google Maps -->
+        <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.2401688176465!2d106.71129553721617!3d-6.232037447254564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fa05d990da31%3A0xbc4ef01caa88d41e!2sMI%20Plus%20Fatahillah!5e0!3m2!1sen!2sid!4v1718485103988!5m2!1sen!2sid" 
+            width="1300" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+</body>
+</html>
+     <section id="tentang" class="section-margin" style="margin-bottom: 100pt;">
+      
+        <div class="row content" id="login">
+      <div class="col-lg-12 danger text-center zero-panel">
+        <div class="col-lg-12 zero-panel-content">
+          <div class="jumbotron">
+          <h1> Tentang E-Catalogue Buku Perpustakaan SMP Fatahillah Ciledug Tangerang </h1><br>
+          <p>E-Catalogue adalah aplikasi katalog perpustakaan berbasis website yang digunakan untuk mencari dan meminjam buku secara digital sehingga pengguna dapat mencari dan meminjam buku dimana saja dan kapan saja tanpa harus pergi ke perpustakaan</p>  
+          </div>
+          
+        
+        </div>
+        <br>
+        
+         
+          
+          
+          
+        
+      </div>
+    </div>
+    </section>
+
+    <!-- footer -->
+
+<footer class="footer container">
+  
+<section class="col-sm-12" style="margin-top: 50px;">
+<div class="col-lg-10 col-lg-offset-1 text-center">
+<br>
+<ul class="list-inline">
+<li>
+<a href="https://www.facebook.com/100010389100463/" target="_blank"><i class="fa fa-facebook fa-fw fa-2x"></i></a>
+</li>
+<li>
+<a href="https://github.com/muhfauzanramsdeenn" target="_blank"><i class="fa fa-github fa-fw fa-2x"></i></a>
+</li>
+<li>
+<a href="https://www.linkedin.com/in/muh-fauzan-74886120a/" target="_blank"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
+</li>
+</ul>
+<hr class="medium">
+<p class="text-muted" style="font-size: 16px;">Copyright &copy;<script>document.write(new Date().getFullYear());</script> Nick Marshall Ramsdenn All rights reserved</p>
+</div>
+</section>
+</footer>
+
+ <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!--include-->
+    <script src="vendor/css/js/bootstrap.min.js"></script>
+</body>
+</html>
